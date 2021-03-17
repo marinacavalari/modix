b97973b8e3f04f82cd216831a27b02b3293107c0
Marina Cavalari <marina.cavalari.mc@gmail.com>
2021-03-16 21:05:08 -0300

    Create Project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to modix
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject modix "0.1.0-SNAPSHOT"
+  :description "A web window to help you catalog and find the best options for your clothes, from the favorite ones till the ones you never imagined of."
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns modix.core})
